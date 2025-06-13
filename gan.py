33958f82b61d472dacc06687e8551880e23792a0
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-06-13 14:08:09 +0200

    creating a discriminator class

--- gan.py
+++ gan.py
@@ -15,3 +15,23 @@ def make_generator_network(input_size, n_filters):
         nn.Tanh()
     )
     return model
+
+class Discriminator(nn.Module):
+    def  __init__(self, n_filters):
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Conv2d(1, n_filters, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(n_filters, n_filters*2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(n_filters * 2),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(n_filters*2, n_filters*4, 3, 2, 1, bias=False),
+            nn.BatchNorm2d(n_filters * 4),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(n_filters*4, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+        def forward(self, input):
+            output = self.network(input)
+            return output.view(-1, 1).squeeze(0)
