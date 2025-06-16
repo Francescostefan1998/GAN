9cc4eea018212fa9832cca04fc0d616294933971
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-06-16 16:03:55 +0200

    creating the WGAN class

---
+++ wGan.py
@@ -0,0 +1,39 @@
+import torch.nn as nn
+import torch
+
+def make_genearator_network_wgan(input_size, n_filters):
+    model = nn.Sequential(
+        nn.ConvTranspose2d(input_size, n_filters*4, 4, 1, 0, bias=False),
+        nn.InstanceNorm2d(n_filters*4),
+        nn.LeakyReLU(0.2),
+        nn.ConvTranspose2d(n_filters*4, n_filters*2, 3, 2, 1, bias=False),
+        nn.InstanceNorm2d(n_filters*2),
+        nn.LeakyReLU(0.2),
+        nn.ConvTranspose2d(n_filters*2, n_filters, 4, 2, 1, bias=False),
+        nn.InstanceNorm2d(n_filters),
+        nn.LeakyReLU(0.2),
+        nn.ConvTranspose2d(n_filters, 1,4,2,1, bias=False),
+        nn.Tanh()
+    )
+    return model
+
+class DiscriminatorWGAN(nn.Module):
+    def __init__(self, n_filters):
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Conv2d(1, n_filters, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(n_filters, n_filters*2, 4, 2, 1, bias=False),
+            nn.InstanceNorm2d(n_filters*2),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(n_filters*2, n_filters*4, 3, 2, 1, bias=False),
+            nn.InstanceNorm2d(n_filters*4),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(n_filters*4, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        output = self.network(input)
+        return output.view(-1, 1).squeeze(0)
+    
