e3c2ac72d58c0ad432a9f093ca0de5f4e46b9442
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-06-18 11:41:21 +0200

    implementing a BasicGraphConvolutionLuyer

--- GNN.py
+++ GNN.py
@@ -9,7 +9,7 @@ import torch.nn.functional as F
 class NodeNetwork(torch.nn.Module):
     def __init__(self, input_features):
         super().__init__()
-        self.conv_1 = BasicGraphConvolutionLayer(input_features, 32)
+        self.conv_1 = BasicGraphConvolutionLayer(input_features, 32) # here is where the magic formula should happen
         self.conv_2 = BasicGraphConvolutionLayer(32, 32)
         self.fc_1 = torch.nn.Linear(32, 16)
         self.out_layer = torch.nn.Linear(16, 2)
@@ -22,5 +22,20 @@ class NodeNetwork(torch.nn.Module):
         output = self.out_layer(output)
         return F.softmax(output, dim=1)
     
-    
+
+class BasicGraphConvolutionLyaer(torch.nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.W2 = Parameter(torch.rand((in_channels, out_channels), dtype=torch.float32))
+        self.W1 = Parameter(torch.rand((in_channels, out_channels), dtype=torch.float32))
+        self.bias = Parameter(torch.zeros(out_channels, dtype = torch.float32))
+    def forward(self, X, A):
+        potential_msgs = torch.mm(X, self.W2)
+        propagated_msgs = torch.mm(A, potential_msgs)
+        root_update = torch.mm(X, self.W1)
+        output = propagated_msgs + root_update + self.bias
+        return output
+
         
