0eebec6f1ab945590365ce0b4e10b2a8989a6789
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-06-20 09:56:39 +0200

    train the GNN to predict molecule's isotropic polarizability

--- GNNPytorchGeometric.py
+++ GNNPytorchGeometric.py
@@ -70,3 +70,63 @@ class ExampleNet(torch.nn.Module):
         x = F.relu(self.fc_1(x))
         output = self.out(x)
         return output
+    
+
+from torch.utils.data import random_split
+train_set, valid_set, test_set = random_split(dset, [110000, 10831, 10000])
+trainloader = DataLoader(train_set, batch_size=32, shuffle=True)
+validloader = DataLoader(valid_set, batch_size=32, shuffle=True)
+testloader = DataLoader(test_set, batch_size=32, shuffle=True)
+
+# initialize network
+qm9_node_feats, qm9_edge_feats = 11, 4
+net = ExampleNet(qm9_node_feats, qm9_edge_feats)
+# initialize an optimizer with some reasonable parameters
+optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
+epochs = 4
+target_idx = 1 # index position of the polarizability label
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+net.to(device)
+
+for total_epochs in range(epochs):
+    epoch_loss = 0
+    total_graphs = 0
+    net.train()
+    for batch in trainloader:
+        batch.to(device)
+        optimizer.zero_grad()
+        output = net(batch)
+        loss = F.mse_loss(output, batch.y[:, target_idx].unsqueeze(1))
+        loss.backward()
+        epoch_loss += loss.item()
+        total_graphs += batch.num_graphs
+        optimizer.step()
+    train_avg_loss = epoch_loss / total_graphs
+    val_loss = 0
+    total_graphs = 0
+    net.eval()
+    for batch in validloader:
+        batch.to(device)
+        output = net(batch)
+        loss = F.mse_loss(output, batch.y[:, target_idx].unsqueeze(1))
+        val_loss += loss.item()
+        total_graphs += batch.num_graphs
+    val_avg_loss = val_loss / total_graphs
+    print(f"Epochs: {total_epochs} | epoch avg. loss: {train_avg_loss:.2f} | validation avg. loss: {val_avg_loss:.2f}")
+
+net.eval()
+predictions = []
+real = []
+for batch in testloader:
+    output = net(batch.to(device))
+    predictions.append(output.detach().cpu().numpy())
+    real.append(batch.y[:, target_idx].detach().cpu().numpy())
+
+real = np.concatenate(real)
+predictions = np.concatenate(predictions)
+
+# plot the first 500 predictions
+import matplotlib.pyplot as plt
+plt.scatter(real[:500], predictions[:, 500])
+plt.xlabel('Isotropic polarizability')
+plt.ylabel('Predicted isotropic polarizability')
